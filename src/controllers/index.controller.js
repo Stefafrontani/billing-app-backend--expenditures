c3ab433308fbd237b1ac76b3a24c940c2fa46a98
stefafrontani <stefafrontani@gmail.com>
2021-08-16 13:16:19 -0300

    [billing-app-backend--expenditures]__configuration: dockerize the application server and db

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,11 +4,11 @@ const { Pool } = require('pg')
 require('dotenv').config({ path: './src/database/.env' })
 
 const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  port: process.env.POSTGRES_PORT
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  database: process.env.POSTGRES_DB
 })
 
 const getExpenditures = async (req, res) => {
